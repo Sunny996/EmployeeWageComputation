a5377ceb29b8ffbc15efa251e21f6a840cbb4a22
AkashSatla <satlaakash@gmail.com>
2022-03-11 19:42:23 +0530

    ADDED: A method to calculate Wage per day for Part time

--- src/main/EmployeeWage.java
+++ src/main/EmployeeWage.java
@@ -5,14 +5,17 @@ public class EmployeeWage {
     public int IS_ABSENT=0;
     public int WAGE_PER_HOUR=20;
     public int FULL_DAY_HOUR=8;
+    public int PART_TIME_HOUR = 4;
+    public int IS_PART_TIME = 2;
 
         public int empDailyWage(){
-        int dailyWage=0;
-        int empPresent=(int)(Math.random()*10)%2;
-        if(empPresent==IS_PRESENT)
-            dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
-        System.out.println("Daily Wage on a Full Day is "+dailyWage);
-            System.out.println(empPresent);
-        return dailyWage;
+            int dailyWage=0;
+            int empPresent=(int)(Math.random()*10)%3;
+            if(empPresent==IS_PRESENT)
+                dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
+            else if(empPresent==IS_PART_TIME)
+                dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
+            System.out.println("Daily Wage is "+dailyWage);
+            return dailyWage;
     }
 }
