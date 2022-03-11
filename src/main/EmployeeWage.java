6d323ad202254dcc4e1eb8a4c07a9e83c9e2a40e
AkashSatla <satlaakash@gmail.com>
2022-03-11 19:51:55 +0530

    ADDED: A method to calculate Employee wage per month

--- src/main/EmployeeWage.java
+++ src/main/EmployeeWage.java
@@ -1,26 +1,32 @@
 package main;
 
 public class EmployeeWage {
-    public int IS_PRESENT=1;
-    public int IS_ABSENT=0;
-    public int WAGE_PER_HOUR=20;
-    public int FULL_DAY_HOUR=8;
+    public int IS_PRESENT = 1;
+    public int IS_ABSENT = 0;
+    public int WAGE_PER_HOUR = 20;
+    public int FULL_DAY_HOUR = 8;
     public int PART_TIME_HOUR = 4;
     public int IS_PART_TIME = 2;
+    public int DAYS_PER_MONTH = 20;
 
-        public int empDailyWage(){
-            int dailyWage=0;
-            int empPresentSwitch=(int)(Math.random()*10)%3;
-            switch(empPresentSwitch){
+    public int empDailyWage() {
+        int totalWage = 0;
+        int totalWorkingDays = 0;
+        while (totalWorkingDays < DAYS_PER_MONTH) {
+            switch ((int) (Math.random() * 10) % 3) {
                 case 1:
-                    dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
+                    totalWage += WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    totalWorkingDays++;
                     break;
                 case 2:
-                    dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
+                    totalWage += WAGE_PER_HOUR * PART_TIME_HOUR;
+                    totalWorkingDays++;
                     break;
 
             }
-            System.out.println("Daily Wage is "+dailyWage);
-            return dailyWage;
+        }
+        System.out.println("Employee Wage Per Month is " + totalWage);
+        return totalWage;
+
     }
 }
