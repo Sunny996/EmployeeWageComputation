e8b25509f2ee278b92f870e6d70166f7d23c8d33
AkashSatla <satlaakash@gmail.com>
2022-03-11 19:46:44 +0530

    ADDED: Switch Case to calculate employee wage

--- src/main/EmployeeWage.java
+++ src/main/EmployeeWage.java
@@ -10,11 +10,16 @@ public class EmployeeWage {
 
         public int empDailyWage(){
             int dailyWage=0;
-            int empPresent=(int)(Math.random()*10)%3;
-            if(empPresent==IS_PRESENT)
-                dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
-            else if(empPresent==IS_PART_TIME)
-                dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
+            int empPresentSwitch=(int)(Math.random()*10)%3;
+            switch(empPresentSwitch){
+                case 1:
+                    dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
+                    break;
+                case 2:
+                    dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
+                    break;
+
+            }
             System.out.println("Daily Wage is "+dailyWage);
             return dailyWage;
     }
