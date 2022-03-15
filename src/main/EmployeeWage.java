74f5a217449e8944c7d9447d80d12dd07133893b
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-03-15 09:09:43 +0530

    Update EmployeeWage.java

--- src/main/EmployeeWage.java
+++ src/main/EmployeeWage.java
@@ -1,21 +1,21 @@
 package main;
 
 public class EmployeeWage {
-    public int IS_PRESENT=1;
-    public int IS_ABSENT=0;
-    public int WAGE_PER_HOUR=20;
-    public int FULL_DAY_HOUR=8;
-    public int PART_TIME_HOUR = 4;
-    public int IS_PART_TIME = 2;
+    public int final IS_PRESENT=1;
+    public int final IS_ABSENT=0;
+    public int final WAGE_PER_HOUR=20;
+    public int final FULL_DAY_HOUR=8;
+    public int final PART_TIME_HOUR = 4;
+    public int final IS_PART_TIME = 2
 
         public int empDailyWage(){
             int dailyWage=0;
             int empPresentSwitch=(int)(Math.random()*10)%3;
             switch(empPresentSwitch){
-                case 1:
+                case IS_PRESENT:
                     dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
                     break;
-                case 2:
+                case IS_PART_TIME:
                     dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
                     break;
 
