233af1a8d6f11f0249d1fc0c3f8507c00353ac58
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-03-15 09:28:23 +0530

    Update EmployeeWage.java

--- src/main/EmployeeWage.java
+++ src/main/EmployeeWage.java
@@ -1,14 +1,14 @@
 package main;
 
 public class EmployeeWage {
-    public int IS_PRESENT = 1;
-    public int IS_ABSENT = 0;
-    public int WAGE_PER_HOUR = 20;
-    public int FULL_DAY_HOUR = 8;
-    public int PART_TIME_HOUR = 4;
-    public int IS_PART_TIME = 2;
-    public int DAYS_PER_MONTH = 20;
-    public int HOURS_PER_MONTH = 100;
+    public int final IS_PRESENT = 1;
+    public int final IS_ABSENT = 0;
+    public int final WAGE_PER_HOUR = 20;
+    public int final FULL_DAY_HOUR = 8;
+    public int final PART_TIME_HOUR = 4;
+    public int final IS_PART_TIME = 2;
+    public int final DAYS_PER_MONTH = 20;
+    public int final HOURS_PER_MONTH = 100;
 
     public int empDailyWage() {
         int totalWage = 0;
