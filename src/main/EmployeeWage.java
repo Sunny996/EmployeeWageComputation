0a33ad6a3980562df4372cd42eb9d601f7b37173
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-03-15 09:11:34 +0530

    Update EmployeeWage.java

--- src/main/EmployeeWage.java
+++ src/main/EmployeeWage.java
@@ -1,24 +1,24 @@
 package main;
 
 public class EmployeeWage {
-    public int IS_PRESENT = 1;
-    public int IS_ABSENT = 0;
-    public int WAGE_PER_HOUR = 20;
-    public int FULL_DAY_HOUR = 8;
-    public int PART_TIME_HOUR = 4;
-    public int IS_PART_TIME = 2;
-    public int DAYS_PER_MONTH = 20;
+     public int final IS_PRESENT=1;
+    public int final IS_ABSENT=0;
+    public int final WAGE_PER_HOUR=20;
+    public int final FULL_DAY_HOUR=8;
+    public int final PART_TIME_HOUR = 4;
+    public int final IS_PART_TIME = 2;
+    public int final DAYS_PER_MONTH = 20;
 
     public int empDailyWage() {
         int totalWage = 0;
         int totalWorkingDays = 0;
         while (totalWorkingDays < DAYS_PER_MONTH) {
             switch ((int) (Math.random() * 10) % 3) {
-                case 1:
+                case IS_PRESENT:
                     totalWage += WAGE_PER_HOUR * FULL_DAY_HOUR;
                     totalWorkingDays++;
                     break;
-                case 2:
+                case IS_PART_TIME:
                     totalWage += WAGE_PER_HOUR * PART_TIME_HOUR;
                     totalWorkingDays++;
                     break;
