a028f70b1f83056f99b33e332dacb59865434b3b
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-04-04 23:52:25 +0530

    Update EmployeeWageTest.java

--- src/main/EmployeeWageTest.java
+++ src/main/EmployeeWageTest.java
@@ -2,9 +2,7 @@ package main;
 
 public class EmployeeWageTest {
     public static void main(String[] args) {
-        EmployeeWage dMart = new EmployeeWage("DMart", 20, 2, 10);
-        dMart.empDailyWage();
-        EmployeeWage reliance = new EmployeeWage("Reliance", 25, 3, 15);
-        reliance.empDailyWage();
+        EmployeeWage empWage=new EmployeeWage();
+        empWage.empDailyWage();
     }
 }
